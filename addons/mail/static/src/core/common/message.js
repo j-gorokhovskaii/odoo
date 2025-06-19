3a25cecd0a02d05d1c0b38fbf2c282f5adfa43d8
j-gorokhovskaii <j.gorokhovskaii@gmail.com>
2025-06-20 11:09:30 +1200

    feat(ui): change system-wide branding color from #71639e to #29abe2 - Update Odoo community color in primary variables - Change theme-color meta tag in web client templates - Update HTML editor table picker active color - Update mail message shadow link colors - Ensure consistent blue branding across all UI elements

--- addons/mail/static/src/core/common/message.js
+++ addons/mail/static/src/core/common/message.js
@@ -57,10 +57,10 @@ import { discussComponentRegistry } from "./discuss_component_registry";
  * @extends {Component<Props, Env>}
  */
 export class Message extends Component {
-    // This is the darken version of #71639e
-    static SHADOW_LINK_COLOR = "#66598f";
+    // This is the darken version of #29abe2
+    static SHADOW_LINK_COLOR = "#1f6feb";
     static SHADOW_HIGHLIGHT_COLOR = "#e99d00bf";
-    static SHADOW_LINK_HOVER_COLOR = "#564b79";
+    static SHADOW_LINK_HOVER_COLOR = "#0056b3";
     static components = {
         ActionSwiper,
         AttachmentList,
